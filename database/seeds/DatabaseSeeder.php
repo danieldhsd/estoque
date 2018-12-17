29135c16a862a8f7c672a89f75c8d7588eda3c62
danieldhsd <danieldhsd@yahoo.com.br>
2018-12-17 12:41:01 -0200

    Inserinso no Banco de dados

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,34 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
     /**
      * Seed the application's database.
      *
      * @return void
      */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
+    public function run(){
+    	Model::unguard();
+
+    	$this->call('ProdutoTableSeeder');
     }
 }
+
+class ProdutoTableSeeder extends Seeder{
+	
+	public function run(){
+			DB::insert('insert into produtos(nome, quantidade, valor, descricao) values (?,?,?,?)',
+			array('Geladeira', 2, 5900.00, 'Side by Side com gelo na porta'));
+
+			DB::insert('insert into produtos(nome, quantidade, valor, descricao) values (?,?,?,?)',
+			array('Fogão', 5, 950.00, 'Painel automático e forno elétrico'));
+
+			DB::insert('insert into produtos (nome, quantidade, valor, descricao)values (?,?,?,?)',
+			array('Microondas', 1, 1520.00,'Manda SMS quando termina de esquentar'));
+	}	
+}
+
+?>
